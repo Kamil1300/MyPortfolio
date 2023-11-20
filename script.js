7384db9ef9ed67c4c2eb5a129144b1dc82cea81e
Shaikh Kamil <Shaikhkamil789@gmail.com>
2023-11-20 17:54:04 +0530

    Changes in script.js and index.js

--- script.js
+++ script.js
@@ -52,7 +52,8 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // typed js 
 const typed = new Typed('.multiple-text', {
-    strings: ['Full stack Developer', 'Front end Developer', 'Back end Developer'],
+    // strings: ['Full stack Developer', 'Front end Developer', 'Back end Developer'],
+    strings: ['Front end Developer', 'Reactjs Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
